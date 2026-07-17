fd9359b6a544bb2fc2d222ceb41ad1eb885882c8
Alaa <joaovitorsp.limadias@gmail.com>
2026-07-17 01:07:30 -0300

    feat: pagina inicial

---
+++ frontend-app/src/app/paginaincial/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './App.module.css';
+
+function page() {
+  return (
+    <div className={styles.texto}>
+        <h1>Página Inicial</h1>
+    </div>
+  )
+}
+
+export default page
